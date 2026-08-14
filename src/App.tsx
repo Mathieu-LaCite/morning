c9c9b77cbe994a4cc74839dce363d4a905578301
Mathieu Larocque <2706544@collegelacite.ca>
2026-08-14 10:13:58 -0400

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import bg from './assets/MorningBg.png'
+
 function App() {
 
   return (
     <>
-      
+      <div className="min-h-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${bg})` }}>
+          <div>
+            <h1>Yes this works or no....</h1>
+            <p>Lets see!</p>
+          </div>
+      </div>
     </>
   )
 }
