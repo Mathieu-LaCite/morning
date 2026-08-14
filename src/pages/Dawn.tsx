dee1e95b325600bcd2653ce3aec8bf73d2050883
Mathieu Larocque <2706544@collegelacite.ca>
2026-08-14 11:37:56 -0400

    made my app.tsx into a router page and moved the content from there into a new page called dawn.tsx

---
+++ src/pages/Dawn.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import bg from '../assets/dawn2.png'
+import Header from '../components/Header'
+import SunSlider from '../components/SunSlider'
+import InfoSidebar from '../components/SideBar'
+
+function App() {
+  const [brightness, setBrightness] = useState(1)
+
+  //this is for the brightness, changes the min/max output based on the equation below
+  const handleSunMove = (percent: number) => {
+    const newBrightness = 0.7 + (percent / 100) * 0.5
+    setBrightness(newBrightness)
+  }
+
+  return (
+    <div
+      className="relative min-h-screen bg-cover bg-center bg-no-repeat transition-[filter] duration-150"
+      style={{
+        backgroundImage: `url(${bg})`,
+        filter: `brightness(${brightness})`,
+      }}
+    >
+      <Header/>
+      <SunSlider onPositionChange={handleSunMove} />
+      <InfoSidebar/>
+    </div>
+  )
+}
+
+export default App
